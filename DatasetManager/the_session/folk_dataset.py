e87b4ab206c2afd803e98ee5653e3dd62e82defa
Ghadjeres <hadjeres.g@gmail.com>
2018-05-14 17:51:03 +0200

    add folk_dataset

---
+++ DatasetManager/the_session/folk_dataset.py
@@ -0,0 +1,38 @@
+from DatasetManager.dataset_manager import DatasetManager
+from DatasetManager.music_dataset import MusicDataset
+import os
+
+
+class FolkDataset(MusicDataset):
+    def __init__(self, cache_dir=None):
+        super(FolkDataset, self).__init__(cache_dir=cache_dir)
+        self.raw_dataset_dir = os.path.join(
+            self.cache_dir,
+            'raw_dataset',
+        )
+        if not os.path.exists(self.raw_dataset_dir):
+            os.mkdir(self.raw_dataset_dir)
+
+        self.raw_dataset_full = os.path.join(
+            self.raw_dataset_dir,
+            'raw_dataset_full.txt'
+        )
+
+        self.raw_dataset_url = 'https://raw.githubusercontent.com/IraKorshunova/' \
+                               'folk-rnn/master/data/' \
+                               'sessions_data_clean.txt'
+
+    def download_raw_dataset(self):
+        if os.path.exists(self.raw_dataset_full):
+            print('The Session dump already exists')
+        else:
+            print('Downloading The Session dump')
+            os.system(f'wget -L {self.raw_dataset_url} -O {self.raw_dataset_full}')
+
+    def make_tensor_dataset(self):
+        pass
+
+if __name__ == '__main__':
+    # dataset_manager = DatasetManager()
+    folk_dataset = FolkDataset(cache_dir='../dataset_cache')
+    folk_dataset.download_raw_dataset()

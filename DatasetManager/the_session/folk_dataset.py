dccdfaa702f261d578ee66e0e5366cb396f23629
Ghadjeres <hadjeres.g@gmail.com>
2018-05-14 17:54:44 +0200

    add split_raw_dataset

--- DatasetManager/the_session/folk_dataset.py
+++ DatasetManager/the_session/folk_dataset.py
@@ -13,7 +13,7 @@ class FolkDataset(MusicDataset):
         if not os.path.exists(self.raw_dataset_dir):
             os.mkdir(self.raw_dataset_dir)
 
-        self.raw_dataset_full = os.path.join(
+        self.full_raw_dataset_filepath = os.path.join(
             self.raw_dataset_dir,
             'raw_dataset_full.txt'
         )
@@ -23,14 +23,33 @@ class FolkDataset(MusicDataset):
                                'sessions_data_clean.txt'
 
     def download_raw_dataset(self):
-        if os.path.exists(self.raw_dataset_full):
+        if os.path.exists(self.full_raw_dataset_filepath):
             print('The Session dump already exists')
         else:
             print('Downloading The Session dump')
-            os.system(f'wget -L {self.raw_dataset_url} -O {self.raw_dataset_full}')
+            os.system(f'wget -L {self.raw_dataset_url} -O {self.full_raw_dataset_filepath}')
+
+    def split_raw_dataset(self):
+        with open(self.full_raw_dataset_filepath) as full_raw_dataset_file:
+            tune_index = 0
+
+            current_song_filepath = os.path.join(self.raw_dataset_dir,
+                                                 f'tune_{tune_index}')
+            current_song_file = open(current_song_filepath, 'w+')
+            for line in full_raw_dataset_file:
+                if line == '\n':
+                    tune_index += 1
+                    current_song_file.flush()
+                    current_song_file.close()
+                    current_song_filepath = os.path.join(self.raw_dataset_dir,
+                                                         f'tune_{tune_index}')
+                    current_song_file = open(current_song_filepath, 'w+')
+                else:
+                    current_song_file.write(line)
 
     def make_tensor_dataset(self):
         pass
+
 
 if __name__ == '__main__':
     # dataset_manager = DatasetManager()

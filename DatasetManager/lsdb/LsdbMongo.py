9c7d62df11fb840597f90984ba8c1e1b66a402be
Ghadjeres <hadjeres.g@gmail.com>
2018-06-11 18:20:45 +0200

    remove tabs

--- DatasetManager/lsdb/LsdbMongo.py
+++ DatasetManager/lsdb/LsdbMongo.py
@@ -5,45 +5,45 @@ from DatasetManager.lsdb.passwords import LOGIN_READONLY, PASSWORD_READONLY, SER
 
 
 class LsdbMongo:
-	def __init__(self):
-		self.server = SSHTunnelForwarder(
-			SERVER_ADDRESS,
-			ssh_username='gaetan',
-			remote_bind_address=('127.0.0.1', 27017)
-		)
-		self.server.start()
+    def __init__(self):
+        self.server = SSHTunnelForwarder(
+            SERVER_ADDRESS,
+            ssh_username='gaetan',
+            remote_bind_address=('127.0.0.1', 27017)
+        )
+        self.server.start()
 
-		self.client = MongoClient('localhost', self.server.local_bind_port)
+        self.client = MongoClient('localhost', self.server.local_bind_port)
 
-	def get_db(self):
-		db = self.client.get_database('lsdb')
-		db.authenticate(LOGIN_READONLY, PASSWORD_READONLY,
-		                mechanism='SCRAM-SHA-1')
-		return db
+    def get_db(self):
+        db = self.client.get_database('lsdb')
+        db.authenticate(LOGIN_READONLY, PASSWORD_READONLY,
+                        mechanism='SCRAM-SHA-1')
+        return db
 
-	def get_songbook_leadsheets_cursor(self, db):
-		"""Return a cursor all songbook leadsheets, excluding user input ones
-		"""
-		return db.leadsheets.find(
-			{'source': {"$ne": "51b6fe4067ca227d25665b0e"}})
+    def get_songbook_leadsheets_cursor(self, db):
+        """Return a cursor all songbook leadsheets, excluding user input ones
+        """
+        return db.leadsheets.find(
+            {'source': {"$ne": "51b6fe4067ca227d25665b0e"}})
 
-	def close(self):
-		self.client.close()
-		self.server.close()
+    def close(self):
+        self.client.close()
+        self.server.close()
 
-	def __del__(self):
-		self.close()
+    def __del__(self):
+        self.close()
 
-	def __enter__(self):
-		return self
+    def __enter__(self):
+        return self
 
-	def __exit__(self, exc_type, exc_val, exc_tb):
-		self.close()
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
 
 
 if __name__ == '__main__':
-	lsdb_client = LsdbMongo()
-	db = lsdb_client.get_db()
-	cursor = lsdb_client.get_songbook_leadsheets_cursor(db)
-	print(next(cursor))
-	lsdb_client.close()
+    lsdb_client = LsdbMongo()
+    db = lsdb_client.get_db()
+    cursor = lsdb_client.get_songbook_leadsheets_cursor(db)
+    print(next(cursor))
+    lsdb_client.close()
